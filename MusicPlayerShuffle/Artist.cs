fe169828aaf533b0513197f62ee970fbf4a6749d
MaryLapko <mary.putilina@gmail.com>
2019-01-18 03:31:19 +0300

    first commit

---
+++ MusicPlayerShuffle/Artist.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer
+{
+    public class Artist { 
+    
+        public enum Genre
+        {
+            Rock,
+            Classic,
+            Trans,
+            Techno,
+            DubStep
+        }
+       
+        public string Name;
+
+        public Artist()
+        {
+            this.Name = "Default artist";           
+        }
+
+        public Artist(string name)
+        {
+            this.Name = name;            
+        }
+
+        //public Artist(string name, string genre)
+        //{
+        //    this.Name = name;
+        //    this.Genre = genre;
+        //}
+    }
+}
